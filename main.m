a362c1a0426287dddf556a17217e2574b4ad96bb
VictorMeimariss <victor_meimaris@outlook.com>
2026-04-13 20:48:04 +0300

    First Commit

---
+++ main.m
@@ -0,0 +1,6 @@
+%% 13/4/26 Victor Emmanuel Meimaris: Created main.m script to recreate the results from 
+%% "Phase-coded FMCW Laser Headlamp for Integrated Sensing, Communication, and Illumination"
+
+clc;
+clear;
+
